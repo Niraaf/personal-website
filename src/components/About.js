6324cf52d1fdc22dd349ef1638930d18f280f371
Farin Soriano <72371352+Niraaf@users.noreply.github.com>
2025-03-31 17:06:40 -0700

    escaped a character

--- src/components/About.js
+++ src/components/About.js
@@ -4,10 +4,10 @@ export default function About() {
             <div className="flex flex-col items-center pt-20 min-h-screen p-4 rounded-3xl bg-[var(--secondary-accent)] text-[var(--accent)]" >
                 <div className="p-6 ">
                     <h1 className="text-7xl font-extrabold mb-4 ">
-                        What's up!
+                        What&apos;s up!
                     </h1>
                     <p className="text-3xl font-medium mb-6">
-                        I'm a current CS major at UC Irvine, and I'm passionate about tech and self-improvement.
+                        I&apos;m a current CS major at UC Irvine, and I&apos;m passionate about tech and self-improvement.
                     </p>
                 </div>
             </div>
